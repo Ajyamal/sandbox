930ed340e147abe43601e9f68cf480c05b715587
EXEC <uya71@mail.goo.ne.jp>
2015-03-23 08:07:40 +0900

    Update Display.js

--- Display.js
+++ Display.js
@@ -9,12 +9,12 @@ b:function() {
     x.innerHTML = x.attributes["$0"].isId;
 }
 //
-<canvas id="$tile" width="100" height="50" style="border:2px solid #E33B87;">
+<canvas id="$0" width="100" height="50" style="border:2px solid #E33B87;">
 The sentense for HTML5.
 </canvas>
 //
 c:function() {
-var c = document.getElementById("tile");
+var c = document.getElementById("$0");
 var ctx = c.getContext("2d");
 ctx.font = "12px Arial";
 ctx.fillText($STRINGS,12,20);
