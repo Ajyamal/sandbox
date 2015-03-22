d04d7fbab464b637d5955fd1e245011f162a02fb
EXEC <uya71@mail.goo.ne.jp>
2015-03-22 23:03:23 +0900

    Update Output.js

--- Output.js
+++ Output.js
@@ -1,11 +1,11 @@
 function aDisplay() {
     document.open();
-    document.write("$tagA",$context","$tagX"); # $context@array
+    document.write("$tagA",$context","$tagX"); # $context@array or var res = "$0".concat("$n"); 
     document.close();
 }
 
 function bDisplay() {
     var x = document.getElementById("$0");
-    x.innerHTML = x.attributes[$0].isId;
+    x.innerHTML = x.attributes["$0"].isId;
 }
 
