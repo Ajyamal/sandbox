8444731a10b8c3953ee011b29fe07af1ebf2be9f
EXEC <uya71@mail.goo.ne.jp>
2015-03-23 07:37:17 +0900

    added: canvas item

--- Display.js
+++ Display.js
@@ -1,11 +1,17 @@
 function aDisplay() {
     document.open();
-    document.write("$tagA",$context","$tagX"); # $context@array or var res = "$0".concat("$n"); 
+    document.write($tagA,$context,$tagX); # $context@array or var res = $0.concat($n); 
     document.close();
 }
 
 function bDisplay() {
-    var x = document.getElementById("$0");
+    var x = document.getElementById($0);
     x.innerHTML = x.attributes["$0"].isId;
 }
 
+function cDisplay() {
+var c = document.getElementById("tile");
+var ctx = c.getContext("2d");
+ctx.font = "12px Arial";
+ctx.fillText($STRINGS,12,20);
+}
