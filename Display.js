4132bc2ef24f9477576f45eb356cc599fc304b5c
EXEC <uya71@mail.goo.ne.jp>
2015-03-23 07:46:53 +0900

    Update Display.js

--- Display.js
+++ Display.js
@@ -1,15 +1,21 @@
-function aDisplay() {
+a:function() {
     document.open();
     document.write($tagA,$context,$tagX); # $context@array or var res = $0.concat($n); 
     document.close();
 }
 
-function bDisplay() {
+b:function() {
     var x = document.getElementById($0);
     x.innerHTML = x.attributes["$0"].isId;
 }
+//
+<canvas id="tile" width="100" height="50" style="border:2px solid #E33B87;">
+The sentense for HTML5.
+</canvas>
+//
 
-function cDisplay() {
+
+c:function() {
 var c = document.getElementById("tile");
 var ctx = c.getContext("2d");
 ctx.font = "12px Arial";
